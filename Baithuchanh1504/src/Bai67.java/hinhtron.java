59e43365df5e98f345eebfb5d597db956c13fed1
MACHQUANGTRUNG <trungb32002@gmail.com>
2022-04-19 08:38:32 +0700

    Baithuchanh1504

---
+++ Baithuchanh1504/src/Bai67.java/hinhtron.java
@@ -0,0 +1,8 @@
+package Bai67.java;
+
+public class hinhtron {
+        public void xuatthongtin() {
+            System.out.println("Day la hinh tron");
+        }
+    }
+
---
+++ Baithuchanh1504/src/Bai67.java/hinhtru.java
@@ -0,0 +1,8 @@
+package Bai67.java;
+
+public class hinhtru extends hinhtron {
+    public void xuatthongtin() {
+        System.out.println("Day la hinh tru");
+    }
+    
+}
